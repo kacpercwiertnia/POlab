c98392d1cba88b754a3e1a4ad6f4beaf07aad058
kacpercwiertnia <kacpercwiertnia02@gmail.com>
2022-11-15 11:22:48 +0100

    lab7 removed try/catch from World.java

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -5,11 +5,6 @@ import javafx.application.Application;
 
 public class World {
     public static void main(String[] args) {
-        try {
-            Application.launch(App.class, args);
-        } catch (IllegalArgumentException exception) {
-            System.out.println(exception);
-            System.exit(1);
-        }
+        Application.launch(App.class, args);
     }
 }
